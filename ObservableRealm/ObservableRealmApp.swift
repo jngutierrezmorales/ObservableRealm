2f6a90b864592439185c636c298653a2b3afbfbc
Juan Gutierrez <juan.gutierrez@auro-group.com>
2024-02-16 14:16:11 +0100

    base

---
+++ ObservableRealm/ObservableRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableRealmApp.swift
+//  ObservableRealm
+//
+//  Created by Juan Gutierrez on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
